940995bf5eaef8472d1f44eb6e91b40a117cb053
aaron-crosbie <aaroncrosbie1994@gmail.com>
2017-04-23 12:50:49 +0000

    Adding functionallity to admin page

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\Compiler\RepeatedPass;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use AppBundle\Entity\Tag;
@@ -26,11 +27,8 @@ class AdminController extends Controller
     {
         //$this->denyAccessUnlessGranted('ROLE_USER', null, 'Unable to access this page!');
 
-        $nurls = $this->getNurls();
-
-
         $templateName = '/admin/index';
-        return $this->render($templateName . '.html.twig', $nurls);
+        return $this->render($templateName . '.html.twig');
 
         // if get here, not logged in,
         // empty flash bag and create flash login first message then redirect
@@ -44,13 +42,49 @@ class AdminController extends Controller
 //        return $this->redirectToRoute('security_login');
     }
 
-    private function getNurls(){
+    /**
+     * @Route("/nurls", name="admin_nurls")
+     */
+    public function nurlAction(Request $request)
+    {
+        $nurls = $this->getNurls();
+
+        $templateName = '/admin/nurls';
+
+        return $this->render($templateName . '.html.twig', $nurls);
+    }
+
+    /**
+     * @Route("/tags", name="admin_tags")
+     */
+    public function tagAction(Request $request)
+    {
+        $tags = $this->getTags();
+
+        $templateName = '/admin/tags';
+
+        return $this->render($templateName . '.html.twig', $tags);
+    }
+
+    private function getNurls()
+    {
         $em = $this->getDoctrine()->getManager();
 
         $nurls = $em->getRepository('AppBundle:Nurl')->findAll();
 
         return array(
             'nurls' => $nurls
+        );
+    }
+
+    private function getTags()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $tags = $em->getRepository('AppBundle:Tag')->findAll();
+
+        return array(
+            'tags' => $tags
         );
     }
 }
